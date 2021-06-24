f77b1f5d35609a0cd17c711ae91fc155ebabf31e
Daniel <daniel.tecle@hotmail.com>
2021-06-24 16:04:57 +0200

    Dinner break

--- part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
+++ part01-Part01_14.BooleanInput/src/main/java/BooleanInput.java
@@ -7,6 +7,9 @@ public class BooleanInput {
         Scanner scanner = new Scanner(System.in);
 
         // write your program here
+        System.out.println("Write something:");
+        boolean bol = Boolean.valueOf(scanner.next());
+        System.out.println("True or false? " + bol);
 
     }
 }
