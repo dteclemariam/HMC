223f4ead62723aa4e84dce1962cca1ca1dcb9628
Daniel <daniel.tecle@hotmail.com>
2021-06-25 15:18:10 +0200

    Little workbreak coding

--- part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
+++ part01-Part01_16.SecondsInADay/src/main/java/SecondsInADay.java
@@ -7,6 +7,10 @@ public class SecondsInADay {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("How many days would you like to convert to seconds?");
+        int days = Integer.parseInt(scanner.next());
+        int secondsPerGivenDays = ((60 * 60) * 24) * days;
+        System.out.println(secondsPerGivenDays);
 
     }
 }
