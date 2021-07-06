1bcb559c4745effcb59329b686efe8abd8b8863e
Daniel <daniel.tecle@hotmail.com>
2021-07-07 00:04:39 +0200

    Done with JP1 - part 2

--- part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -6,5 +6,16 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Give a number: ");
+        int last = Integer.valueOf(scanner.next());
+        int start = 1;
+        int result = 1;
+
+        while (start<= last){
+            result *= start;
+            start++;
+        }
+        System.out.print("Factorial: " + result);
+
     }
 }
--- part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -13,6 +13,33 @@ public class RepeatingBreakingAndRemembering {
         // the parts you haven't done, but you'll get points for the finished parts.
         
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a numbers:");
+        int currentNumb = 0;
+        int sum = 0;
+        int count = 0;
+        double avg = 0.0;
+        int even = 0;
+        int odd = 0;
+
+        while (true){
+            currentNumb = Integer.valueOf(scanner.next());
+            if (currentNumb == -1){
+                break;
+            }else if (currentNumb % 2 == 0){
+                even++;
+            }else{
+                odd++;
+            }
+            sum += currentNumb;
+            count++;
+        }
+
+        System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + count);
+        System.out.println("Average: " + ((double)sum/count));
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
 
     }
 }
